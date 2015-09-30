f04c80ef993777ffcefec0bc6725f535e6bab031
Naaz <marziyeh.naaz.ahmed@gmail.com>
2015-09-30 14:44:07 +0100

    running server and tests

---
+++ server.js
@@ -0,0 +1,23 @@
+var http = require('http');
+var port= process.env.PORT || 3000;
+// var auto = require('./main.js');
+var fs = require('fs');
+var index = fs.readFileSync(__dirname + '/index.html');
+
+
+function handler(request,response){
+  var url = request.url;
+  console.log("request.url:", url);
+  if (url.length === 1){
+    response.writeHead(200, {"Content-Type":"text/html"});
+    response.end(index.toString());
+  }
+else {response.end("URL IS MORE THAN 1");
+  }
+}
+
+http.createServer(handler).listen(port);
+
+console.log('node http server listening on http://localhost:' + port);
+
+module.exports = handler; //the function that we want to be exported when we call require('server');
