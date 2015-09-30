9e47e63d442644b83fa41390de8fb83aca2aeb71
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2015-09-30 20:27:12 +0100

    fixed lots more typos in server.js

--- server.js
+++ server.js
@@ -9,30 +9,31 @@ var Server = (function() {
   function handler(request,response){
     var url = request.url;
     console.log("request.url:", url);
-    if (url.match(/^(test)/)) {
+    if (url.match(/^(\/test)/)) {
       serveTest(request, response);
     } else if (url.length === 1){
       response.writeHead(200, {"Content-Type":"text/html"});
       response.end(index.toString());
     } else {
       response.writeHead(404, {"Content-Type":"text/javascript"});
-      response.end("error: file not found");
+      response.end("error: " + request.url + " not found");
     }
   }
 
   function serveTest(req, res) {
+    console.log("serveTest called")
     var test  = fs.readFileSync(__dirname + '/tests/front-end/test.html');
     var testjs  = fs.readFileSync(__dirname + '/tests/front-end/test.js');
-    if (url === "/test.html/"){
+    if (req.url === "/test.html/"){
       // console.log("test.html served");
-      response.writeHead(200, {"Content-Type":"text/html"});
-      response.end(test.toString());
-    } else if (url === "/test.html/test.js"){
-      response.writeHead(200, {"Content-Type":"text/javascript"});
-      response.end(testjs.toString());
+      res.writeHead(200, {"Content-Type":"text/html"});
+      res.end(test.toString());
+    } else if (req.url === "/test.html/test.js"){
+      res.writeHead(200, {"Content-Type":"text/javascript"});
+      res.end(testjs.toString());
     } else {
-      response.writeHead(404, {"Content-Type":"text/javascript"});
-      response.end("error: test not found");
+      res.writeHead(404, {"Content-Type":"text/javascript"});
+      res.end("error: " + req.url + " not found");
     }
   }
   function startServer() {
