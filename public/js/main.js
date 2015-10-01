9995b34d785c174d1aa805b1003328d7119ab503
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2015-10-01 14:36:05 +0100

    started keyboard listener on front

--- public/js/main.js
+++ public/js/main.js
@@ -11,9 +11,14 @@ var front = (function() {
   var requestState = false
   var button = document.getElementsByTagName('button')[0];
   var input = document.getElementsByTagName('input')[0];
+  var lastLength = 0;
 
-  var findWordsQuery = function(word) {
+  var dictionaryQuery = function(word) {
     return "/find/:" + word.replace(/[^\w+]/g, '');
+  };
+
+  var wordsQuery = function(word) {
+    return "/words/:" + word.replace(/[^\w+]/g, '');
   };
 
   button.addEventListener('click', function() {
@@ -26,9 +31,23 @@ var front = (function() {
       }
     }
     req.send();
-
-
   });
+
+  input.addEventListener('keypressed', function(e) {
+    var inputTxt = input.value;
+    if (lastLength === 2 && inputTxt.length === 3) {
+      var query = wordsQuery(input.value);
+      var req = new XMLHttpRequest();
+      req.open('GET', query);
+      req.onreadystatechange = function() {
+        if (req.readystate === 4 && req.status === 200) {
+          console.log(req.payload);
+        }
+      }
+      req.send();
+    }
+    lastLength = inputTxt.length;
+  })
 
   return {
     requestState : requestState;
